758d33198bb4dbf58ab072ac1a513b8ee6a80285
Berkay Akbas <berkayakbas@gmail.com>
2022-10-06 16:27:51 +0300

    Implement code for the failed test

--- ruby/solver.rb
+++ ruby/solver.rb
@@ -8,4 +8,11 @@ class Solver
   def reverse(string)
     string.reverse
   end
+
+  def fizzbuzz(number)
+    return 'fizzbuzz' if (number % 5).zero? && (number % 3).zero?
+    return 'fizz' if (number % 3).zero?
+    return 'buzz' if (number % 5).zero?
+    number.to_s
+  end
 end
